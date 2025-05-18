8bb3678540536438b545ae21056b1d78d45e42d8
Satyamk001 <satyamkmr37@gmail.com>
2025-05-18 19:17:28 +0530

    navbar

--- dashboard/src/app/app.module.ts
+++ dashboard/src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './utils/navbar/navbar.component';
+import { FilterTabComponent } from './utils/filter-tab/filter-tab.component';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FilterTabComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    FormsModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ dashboard/src/app/utils/filter-tab/filter-tab.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-filter-tab',
+  templateUrl: './filter-tab.component.html',
+  styleUrls: ['./filter-tab.component.scss']
+})
+export class FilterTabComponent {
+   searchValue = '';
+}
