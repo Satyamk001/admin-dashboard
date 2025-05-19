56521fad6129af29c4e4fa4614bc1a1421e5ab30
Satyamk001 <satyamkmr37@gmail.com>
2025-05-19 17:22:00 +0530

    trending-coins

--- dashboard/src/app/pages/home/home.component.ts
+++ dashboard/src/app/pages/home/home.component.ts
@@ -7,4 +7,36 @@ import { Component, EventEmitter, Output } from '@angular/core';
 })
 export class HomeComponent {
  walletPrice='$10,200.00'
+ cryptocurrencies = [{
+  icon : 'https://www.blockchain.com/explorer/_next/static/media/bitcoin.df7c9480.svg',
+  name : 'Bitcoin',
+  shortName : 'BTC',
+  price : '$102,976',
+  change : '-0.83%',
+  button :'Trade' 
+ },
+{
+  icon : 'https://www.blockchain.com/explorer/_next/static/media/bitcoin.df7c9480.svg',
+  name : 'Bitcoin',
+  shortName : 'BTC',
+  price : '$102,976',
+  change : '-0.83%',
+  button :'Trade' 
+ },
+{
+  icon : 'https://www.blockchain.com/explorer/_next/static/media/bitcoin.df7c9480.svg',
+  name : 'Bitcoin',
+  shortName : 'BTC',
+  price : '$102,976',
+  change : '-0.83%',
+  button :'Trade' 
+ },
+{
+  icon : 'https://www.blockchain.com/explorer/_next/static/media/bitcoin.df7c9480.svg',
+  name : 'Bitcoin',
+  shortName : 'BTC',
+  price : '$102,976',
+  change : '-0.83%',
+  button :'Trade' 
+ },]
 }
