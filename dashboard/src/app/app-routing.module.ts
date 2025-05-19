6d2ebff34d3d5707fbbb871873c9d1388d0d154e
Satyamk001 <satyamkmr37@gmail.com>
2025-05-19 15:09:53 +0530

    routerlink

--- dashboard/src/app/app-routing.module.ts
+++ dashboard/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AnalyticsComponent } from './pages/analytics/analytics.component';
+import { SubscriptionComponent } from './pages/subscription/subscription.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home', component:HomeComponent},
+  {path:'analytics',component:AnalyticsComponent},
+  {path:'subscription',component:SubscriptionComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
