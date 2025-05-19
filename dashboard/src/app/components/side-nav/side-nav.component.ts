151dbe60f22c43a2b5753723fbf4fe3bed1021a0
Satyamk001 <satyamkmr37@gmail.com>
2025-05-19 18:06:44 +0530

    price-card

--- dashboard/src/app/components/side-nav/side-nav.component.ts
+++ dashboard/src/app/components/side-nav/side-nav.component.ts
@@ -1,15 +1,23 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-side-nav',
   templateUrl: './side-nav.component.html',
   styleUrls: ['./side-nav.component.scss']
 })
-export class SideNavComponent {
+export class SideNavComponent implements OnInit {
+  constructor(private activatedRoute : ActivatedRoute, private router: Router){}
   sidebarExpandable = true;
-  activeRoute : any = 'home';
+  activeRoute : any = '';
+
+  ngOnInit(): void {
+   
+  }
+ 
    ActiveNavLink(routeName:string): void{
     this.activeRoute = routeName;
     
   }
+
 }
