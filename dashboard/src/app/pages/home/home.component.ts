c4c4b1c2711cd09d41ea39556ae85a25cb5893fa
ShikharJoshi98 <shikharjoshi89@gmail.com>
2025-05-19 17:23:44 +0530

    wallet Section update

--- dashboard/src/app/pages/home/home.component.ts
+++ dashboard/src/app/pages/home/home.component.ts
@@ -6,5 +6,7 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
- walletPrice='$10,200.00'
+  walletPrice = '$10,200.00';
+  priceUpdate = '$95.23';
+  percentageUpdate = 0.92;
 }
