165a84299b61d803a24f05b4c626b442872f559e
ShikharJoshi98 <shikharjoshi89@gmail.com>
2025-05-20 18:00:20 +0530

    analytics page started

--- dashboard/src/app/pages/analytics/analytics.component.ts
+++ dashboard/src/app/pages/analytics/analytics.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./analytics.component.scss']
 })
 export class AnalyticsComponent {
-
+  currency = '$';
+  price = '105,000';
+  durationArray = ['1D', '1W', '1M', '1Y', 'MAX'];
+  volume = '36,550,790,773';
+  cryptoCurrency = 'BTC';
+  cryptoImage = '/assets/bitcoin-logo-svgrepo-com.svg'
 }
